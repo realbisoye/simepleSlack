d3d2e6b01dace562f9c3c3eb39f6ea8405a603f7
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2019-04-11 08:51:10 -0400

    complete main ui

--- app/components/main/index.js
+++ app/components/main/index.js
@@ -1,17 +1,55 @@
 import React from 'react'
-import {SafeAreaView, View, Text} from 'react-native'
-import AppBar from '..//appBar'
+import {SafeAreaView, ScrollView, StatusBar, View, Text} from 'react-native'
+import AppBar from '../appBar'
+import CommentBox from '../commentBox'
+import Comment from '../comment'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import styles from './styles'
 
-const Main = () => {
-  return (
-    <SafeAreaView style={styles.wrapper}>
-      <AppBar />
-      <View style={styles.wrapper}>
-       <Text style={styles.texts}> Hi </Text>
-      </View>
-    </SafeAreaView>
-  )
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: [],
+    }
+    this.addMessage = this.addMessage.bind(this)
+  }
+
+  addMessage(message) {
+    const messages = this.state.messages
+    const time = new Date().toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+    })
+    messages.push({
+      message,
+      time,
+    })
+    this.setState({messages})
+  }
+
+  render() {
+    const {messages} = this.state
+    return (
+      <SafeAreaView style={styles.main}>
+        <StatusBar backgroundColor="#3F0F3F" barStyle="light-content" />
+        <AppBar />
+        <View style={styles.contentWrapper}>
+          <View style={styles.memberNameSection}>
+            <View style={styles.memberNameWrapper}>
+              <Icon name={'check-circle'} color={'#006D56'} size={15}/>
+              <Text style={styles.memberName}>{'Jon Mctay'}</Text>
+            </View>
+            <Icon name={'keyboard-arrow-down'} color={'#808080'} size={20}/>
+          </View>
+          <ScrollView contentContainerStyle={styles.commentsContainer}>
+            {Boolean(messages.length) && messages.map((m) => <Comment key={m.message} message={m.message} time={m.time}/>)}
+          </ScrollView>
+        </View>
+        <CommentBox addMessage={this.addMessage}/>
+      </SafeAreaView>
+    )
+  }
 }
 
 export default Main
--- app/components/main/styles.js
+++ app/components/main/styles.js
@@ -1,17 +1,44 @@
 import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
+  commentsContainer: {
+    alignItems: 'flex-start',
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingVertical: 25,
+  },
+  contentWrapper: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    flexDirection: 'column',
+    height: '100%',
+  },
+  main: {
+    backgroundColor: '#3F0F3F',
+    flex: 1,
+    flexDirection: 'column',
+  },
+  memberName: {
+    marginLeft: 10,
+  },
+  memberNameSection: {
+    backgroundColor: '#DFDFDF',
+    borderBottomWidth: 0.2,
+    borderColor: '#808080',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+    width: '100%',
+    zIndex: 1000,
+  },
+  memberNameWrapper: {
+    flexDirection: 'row',
+  },
   texts: {
-    color: colors.primaryText,
     fontSize: 30,
     fontWeight: 'bold',
     zIndex: 1000,
-  },
-  wrapper: {
-    alignItems: 'center',
-    backgroundColor: colors.appBarDark,
-    flex: 1,
-    flexDirection: 'column',
   },
 })
 
